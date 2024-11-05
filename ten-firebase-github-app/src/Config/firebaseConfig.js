f5ca74a43eabc4ba9237c85e407660b27b0c1c75
kedharnadhG <g.kedharnadh098@gmail.com>
2024-11-05 13:18:24 +0530

    User_GitHubRepos_using_FirebaseAuth

---
+++ ten-firebase-github-app/src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export default {
+    apiKey: "AIzaSyD2syZcD20qVckcbV21lewRfMCt6B5eo_s",
+    authDomain: "mygitapp-cdd37.firebaseapp.com",
+    projectId: "mygitapp-cdd37",
+    storageBucket: "mygitapp-cdd37.firebasestorage.app",
+    messagingSenderId: "27436677278",
+    appId: "1:27436677278:web:cb66243ea59c9f3aa5238f",
+    measurementId: "G-JMC0G5909D"
+  };
